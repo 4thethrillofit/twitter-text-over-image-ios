0b3eb10a1fbc5a47948889740479d7f4214b2087
Feifan Wang <feifanw@gmail.com>
2015-01-21 00:08:06 -0800

    sync text with output canvas. placeholder for both textViews

--- Spruce/ViewController.swift
+++ Spruce/ViewController.swift
@@ -9,24 +9,66 @@
 import UIKit
 import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var tweetBodyTextView: UITextView!
+    @IBOutlet weak var tweetImageView: UIImageView!
+    @IBOutlet weak var headlineOverlayLabel: UILabel!
+    @IBOutlet weak var headlineTextView: UITextView!
     
+    let headlineTextViewPlaceholder: String = "Type the message you want over the photo here"
+    let tweetBodyTextViewPlaceholder: String = "Type your tweet here"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        headlineTextView.delegate = self
+        tweetBodyTextView.delegate = self
+        headlineOverlayLabel.text = "Make your message beautiful, simply type."
+        headlineTextView.text = headlineTextViewPlaceholder
+        tweetBodyTextView.text = tweetBodyTextViewPlaceholder
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // UITextViewDelegate Methods
+    func textViewDidChange(textView: UITextView) {
+        if textView.restorationIdentifier == "headlineTextView" {
+            headlineOverlayLabel.text = headlineTextView.text
+        } else if textView.restorationIdentifier == "tweetBodyTextView" {
+            println("tweet body")
+        }
+    }
+    
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+        if textView.restorationIdentifier == "headlineTextView"
+        && textView.text == headlineTextViewPlaceholder {
+            textView.text = ""
+        } else if textView.restorationIdentifier == "tweetBodyTextView"
+            && textView.text == tweetBodyTextViewPlaceholder
+        {
+            textView.text = ""
+        }
+        return true
+    }
+    
+    func textViewShouldEndEditing(textView: UITextView) -> Bool {
+        if textView.restorationIdentifier == "headlineTextView"
+            && textView.text == ""
+        {
+            textView.text = headlineTextViewPlaceholder
+        } else if textView.restorationIdentifier == "tweetBodyTextView"
+            && textView.text == ""
+        {
+            textView.text = tweetBodyTextViewPlaceholder
+        }
+        return true
     }
     
     @IBAction func twitterSignInPressed(sender: UIButton) {
         var shareOnTwitter: SLComposeViewController =
             SLComposeViewController(forServiceType: SLServiceTypeTwitter)
-        shareOnTwitter.setInitialText("TWEEEEET")
-        shareOnTwitter.addImage(UIImage(named: "fei_img.jpg"))
+        shareOnTwitter.setInitialText(tweetBodyTextView.text)
+        shareOnTwitter.addImage(tweetImageView.image)
         presentViewController(shareOnTwitter, animated: true, completion: nil)
     }
 }
