0b0f5158cc2a287c2f566579d4e3178e2c8f48da
Feifan Wang <feifanw@gmail.com>
2015-01-20 19:42:56 -0800

    twitter sharing

--- Spruce/ViewController.swift
+++ Spruce/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func twitterSignInPressed(sender: UIButton) {
+        var shareOnTwitter: SLComposeViewController =
+            SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        shareOnTwitter.setInitialText("TWEEEEET")
+        shareOnTwitter.addImage(UIImage(named: "fei_img.jpg"))
+        presentViewController(shareOnTwitter, animated: true, completion: nil)
+    }
 }
-
